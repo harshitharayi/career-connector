7459d82cc001c298bc6632815ba8bee206df6bd0
harshitharayi <harshitha20056@gmail.com>
2024-06-29 05:19:19 -0700

    Initial commit

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By Jobzee</div>
+      <div>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
